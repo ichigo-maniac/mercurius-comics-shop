bd88826295a7b7d522ec7907ab0b3c5262f97d8e
impi <ichigomaniac@gmail.com>
2017-03-08 09:55:20 +0300

    Changed module name

--- mercurius-comisc-shop-web/src/main/java/net/sample/comics/shop/configuration/SessionInitializer.java
+++
@@ -1,9 +0,0 @@
-package net.sample.comics.shop.configuration;
-
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-/**
- * Session initializer
- */
-public class SessionInitializer extends AbstractHttpSessionApplicationInitializer {
-}
