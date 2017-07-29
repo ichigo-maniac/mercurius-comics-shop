c45c9ce1dea32c56a1847cf3de49f9a836386838
impi <ichigomaniac@gmail.com>
2017-07-29 10:02:41 +0300

    MFS-3 Prices and products availability data

--- mercurius-comics-shop-data-import/src/main/java/net/sample/comics/shop/dataimport/ComicsShopPackageDataImportService.java
+++ mercurius-comics-shop-data-import/src/main/java/net/sample/comics/shop/dataimport/ComicsShopPackageDataImportService.java
@@ -58,6 +58,9 @@ public class ComicsShopPackageDataImportService implements PackageDataImportServ
         logResult.append(dataImportService.importClasspathData(RESOURCE_PATH + "prices/catalog-products-videogames-consoles-nintendo-prices.xml"));
         logResult.append(dataImportService.importClasspathData(RESOURCE_PATH + "prices/catalog-products-videogames-ps4-prices.xml"));
         logResult.append(dataImportService.importClasspathData(RESOURCE_PATH + "prices/catalog-products-videogames-xbox-one-prices.xml"));
+        logResult.append(dataImportService.importClasspathData(RESOURCE_PATH + "prices/catalog-products-comics-books-prices.xml"));
+        logResult.append(dataImportService.importClasspathData(RESOURCE_PATH + "prices/catalog-products-videogames-nintendo-switch-prices.xml"));
+        logResult.append(dataImportService.importClasspathData(RESOURCE_PATH + "prices/catalog-products-videogames-nintendo-3ds-prices.xml"));
 
         /** Index */
         logResult.append(dataImportService.importClasspathData(RESOURCE_PATH + "index/index-properties.xml"));
